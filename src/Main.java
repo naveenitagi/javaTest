9c55f51ace21858e0254e209dd35de418106b4e8
Naveen Itagi <naveenitagi@gmail.com>
2025-02-21 20:48:36 +0530

    This is first java commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num [] = {2,3,1,7,6};
+        int temp=0;
+        for(int i=0;i<num.length;i++){
+            for(int j=i+1;j<num.length;j++) {
+                if (num[i] > num[j]) {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+
+        }
+        System.out.println("The sec highest number is :" +num[num.length-2]);
+    }
+}
